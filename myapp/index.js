6af1c45ddde1b26ddcc47b203af71bbd18210742
kevy60 <kevin.vorsmann@voco.ee>
2023-09-26 11:27:09 +0300

    Initial commit

---
+++ myapp/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+// use html view files
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'views'))) 
+
+// show form data in request
+const parseUrl = require('body-parser');
+let encodeUrl = parseUrl.urlencoded({ extended: true });
+
+app.get('/', (req, res) => {
+  console.log('test')
+  console.log(path.join(__dirname, 'validate_form.html'))
+  res.sendFile(path.join(__dirname, 'validate_form.html'))
+})
+
+
+const validId = require('./validate')
+
+app.post('/validate', encodeUrl, (req, res) => {
+  console.log('form data validation')
+  console.log(req.body)
+  console.log(req.body.id_code)
+  res.send(validId.idInfo(req.body.id_code))
+})
+
+app.listen(3000, () => {
+  console.log('Example app is started at http://localhost:3000')
+})
